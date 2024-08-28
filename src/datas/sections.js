3f2d956822579903a0b795723d2733e826e43608
cedricbeau <cedric7693@hotmail.fr>
2024-08-28 20:21:59 +0200

    Update homepage content

---
+++ src/datas/sections.js
@@ -0,0 +1,34 @@
+const sections = [
+    {
+        href: '/actions',
+        title: 'Actions',
+        label: 'Actions',
+    },
+    {
+        href: '/data-display',
+        title: 'Data display',
+        label: 'Data display',
+    },
+    {
+        href: '/navigation',
+        title: 'Navigation',
+        label: 'Navigation',
+    },
+    {
+        href: '/feedback',
+        title: 'Feedback',
+        label: 'Feedback',
+    },
+    {
+        href: '/data-input',
+        title: 'Data-input',
+        label: 'Data-input',
+    },
+    {
+        href: '/layout',
+        title: 'Layout',
+        label: 'Layout',
+    },
+];
+
+export default sections;
